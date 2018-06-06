74409b1c85fb6c67003c4724ccabffbfc223cb58
Jo Robert Cutajar <githubcommit@robajz.info>
2018-06-07 00:01:49 +0200

    Fixing nuget push command

--- publish.sh
+++ publish.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 set -e
 
-nuget push '**/*.nupkg' "$1" -source https://www.nuget.org/api/v2/package
-#https://api.nuget.org/v3/index.json
+nuget push '**/*.nupkg' "$1" -source https://api.nuget.org/v3/index.json
