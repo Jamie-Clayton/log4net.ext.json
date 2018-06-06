9f51be1a0dedfa2220b09af937a65577d8e4e5aa
Jo Robert Cutajar <githubcommit@robajz.info>
2018-06-06 23:42:56 +0200

    Fixing nuget api url

--- publish.sh
+++ publish.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 
-nuget push '**/*.nupkg' "$1" -Source https://api.nuget.org/v3/index.json
+nuget push '**/*.nupkg' "$1" -source https://www.nuget.org/api/v2/package
+#https://api.nuget.org/v3/index.json
